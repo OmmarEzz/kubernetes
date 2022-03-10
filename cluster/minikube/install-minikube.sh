2f0641fe66c5283366439001f72e57b56f166bfa
Ubuntu <ubuntu@ip-172-31-36-153.us-east-2.compute.internal>
2022-03-10 16:46:13 +0000

    cluster setup

--- cluster/minikube/install-minikube.sh
+++ cluster/minikube/install-minikube.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo 'Install Kubectl'
 
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s 
-https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo apt-get update && sudo apt-get install -y apt-transport-https
 
-chmod +x ./kubectl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
 
-sudo mv ./kubectl /usr/local/bin/kubectl
+sudo apt-get install -y kubectl
+
 
 echo 'Step 1 — Installing Docker'
 
@@ -14,7 +16,7 @@ sudo apt update
 
 
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -23,14 +25,14 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 
 echo 'Install minikube'
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-
+sudo usermod -aG docker ubuntu
 echo 'start minikube'
 
 minikube start
