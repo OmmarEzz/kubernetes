8281465cc75c117984bf835f8fcb29db61bd532d
OmarEzzat <101343651+OmmarEzz@users.noreply.github.com>
2022-10-29 13:00:17 +0200

    Update install-node.sh

--- cluster/kubeadm/install-node.sh
+++ cluster/kubeadm/install-node.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "initializing k8s"
 
-kubeadm init  --upload-certs
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
+kubeadm init
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
